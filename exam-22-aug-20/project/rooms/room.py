9cdab2718faafa5bd3bda117261ad7f47cfecf9a
Katerina Mitova <katerinamitova@gmail.com>
2022-08-02 13:48:36 +0300

    Add files via upload

---
+++ exam-22-aug-20/project/rooms/room.py
@@ -0,0 +1,34 @@
+from project.appliances.appliance import Appliance
+from project.people.child import Child
+
+
+class Room:
+    def __init__(self, name: str, budget: float, members_count: int):
+        self.family_name = name
+        self.budget = budget
+        self.members_count = members_count
+        self.children = []
+        self.expenses = 0
+
+    @property
+    def expenses(self):
+        return self.__expenses
+
+    @expenses.setter
+    def expenses(self, value):
+        if value < 0:
+            raise ValueError("Expenses cannot be negative")
+        self.__expenses = value
+
+    def calculate_expenses(self, *args):
+        result = 0
+        for list_el in args:
+            for el in list_el:
+                if isinstance(el, Appliance):
+                    result += el.get_monthly_expense()
+                elif isinstance(el, Child):
+                    result += el.cost * 30
+        self.expenses = result
+
+
+
